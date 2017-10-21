318012032b30956c71bd1ffee399fd3f3ae66206
pixafera <nicholas@pixa.org.uk>
2017-10-21 23:59:57 +0100

    Varying numbers of coins

--- Geobudget/app/src/main/java/com/geobudget/geobudget/MainBudgetItemAdapter.java
+++ Geobudget/app/src/main/java/com/geobudget/geobudget/MainBudgetItemAdapter.java
@@ -1,6 +1,13 @@
 package com.geobudget.geobudget;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,6 +41,21 @@ public class MainBudgetItemAdapter extends ArrayAdapter<Budget> {
 
         TextView totalBudget = (TextView) rowView.findViewById(R.id.totalBudget);
         totalBudget.setText(String.format("£%.2f", values.get(position).getAllowance()));
+
+        ImageView centreCoin = (ImageView) rowView.findViewById(R.id.centrecoin);
+        Resources res = context.getResources();
+        Bitmap oneCentreCoin = BitmapFactory.decodeResource(res, R.drawable.coin_bar_middle);
+        Bitmap bmp = Bitmap.createBitmap(oneCentreCoin.getWidth() * (position + 1), oneCentreCoin.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas cv = new Canvas(bmp);
+        for(int i = 0;i<(position + 1);i++) {
+            cv.drawBitmap(oneCentreCoin, i * oneCentreCoin.getWidth(), 0, null);
+        }
+
+        ConstraintLayout.LayoutParams centreCoinLayout = (ConstraintLayout.LayoutParams) centreCoin.getLayoutParams();
+        float centreCoinWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 9, res.getDisplayMetrics()) * (position + 1);
+        centreCoinLayout.width = (int)centreCoinWidth;
+        centreCoin.setLayoutParams(centreCoinLayout);
+        centreCoin.setImageBitmap(bmp);
         return rowView;
     }
 }
