a131c22d4e559b134c65b726c03d8edcb84c74c2
andyjoelsimpson <andyjoelsimpson@gmail.com>
2017-10-21 22:15:44 +0100

    XML Changes to activity

---
+++ Geobudget/app/src/main/java/com/geobudget/geobudget/BudgetEdit.java
@@ -0,0 +1,13 @@
+package com.geobudget.geobudget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BudgetEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_budget_item);
+    }
+}
---
+++ Geobudget/app/src/main/java/com/geobudget/geobudget/CategoryItem.java
@@ -0,0 +1,13 @@
+package com.geobudget.geobudget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_item);
+    }
+}
